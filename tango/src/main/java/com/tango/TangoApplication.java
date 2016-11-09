8f628249c0d1dae935d893a71b3ec5593d40e4eb
candy.tam <candy.tam@SH-CODE-PC0084.fn.com>
2016-11-09 20:07:23 +0800

    mybatis

---
+++ tango/src/main/java/com/tango/TangoApplication.java
@@ -0,0 +1,29 @@
+package com.tango;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@ServletComponentScan
+@SpringBootApplication
+@EnableTransactionManagement 
+@EnableAspectJAutoProxy
+@EnableWebMvc
+@EnableCaching
+@ComponentScan(basePackages = {"com.tango"}, excludeFilters = {@Filter(type = FilterType.ANNOTATION, value = Controller.class)})
+public class TangoApplication {
+    
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TangoApplication.class, args);
+	}
+	
+}
