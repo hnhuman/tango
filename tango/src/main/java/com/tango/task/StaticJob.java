d13ed091b22d8ca8cfe1e1a2f7e49dfc6375388e
candy.tam <candy.tam@SH-CODE-PC0084.fn.com>
2016-11-12 14:25:14 +0800

    task

--- tango/src/main/java/com/tango/task/StaticJob.java
+++ tango/src/main/java/com/tango/task/StaticJob.java
@@ -1,7 +1,5 @@
 package com.tango.task;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,8 +23,7 @@ public class StaticJob {
 	/**
 	 * 生成货品静态(即时)
 	 */
-//	@Scheduled(fixedDelayString = "${job.static_generate_eager_goods.delay}")
-	@PostConstruct
+	@Scheduled(fixedDelayString = "${job.static_generate_eager_goods.delay}")
 	public void generateEagerGoods() {
 		staticService.generateGoods();
 	}
